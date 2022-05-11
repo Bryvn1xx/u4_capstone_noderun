be84082d93cc92c069853689da377ebda40e7a91
Bryan <bryannsanchez1234@gmail.com.com>
2022-05-11 14:17:38 -0700

    post changes

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,6 +4,8 @@ import { useNavigate, Link } from 'react-router-dom'
 const PostCard = ({ post }) => {
   let navigate = useNavigate()
 
+
+
   const deletePost = async (postId) => {
     await axios.delete(`http://localhost:3001/api/posts/${postId}`)
     navigate('/about')
@@ -20,6 +22,7 @@ const PostCard = ({ post }) => {
         <div className='homepage-buttons'>
           <button className='dlt-btn' onClick={() => deletePost(post.id)}>Delete</button>
           <Link className='edt-btn' to={`/updatepost/${post.id}`}>Edit</Link>
+
         </div>
       </div>
     </div>
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -46,6 +46,7 @@ const PostPage = () => {
   }
 
 
+
   useEffect(() => {
     getCommentsByPost()
     getPost()
