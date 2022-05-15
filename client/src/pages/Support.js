6007ff1c70422b1f0102fd7a4daf5895ac6b94d5
Bryan <bryannsanchez1234@gmail.com.com>
2022-05-15 16:33:07 -0700

    support page

--- client/src/pages/Support.js
+++ client/src/pages/Support.js
@@ -7,10 +7,14 @@ const Support = () => {
         <p className='support-quote'>
           NodeRun ensures that you can get your project up and running as soon as possible, contact one of our developers down below if you are still facing issues!
         </p>
+        <div className='support-socials'>
+          <img className='support-img' src='https://previews.123rf.com/images/djvstock/djvstock1508/djvstock150806855/44096519-web-entwickler-design-vector-illustration-eps-10-.jpg'></img>
+        </div>
         <p2 className='support-socials'>
           Full-Stack Software Engineer:
         <a href='https://www.linkedin.com/in/bryvn1xx/'>Bryan Sanchez</a>
         </p2>
+
       </div>
 
 
