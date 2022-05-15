db8245af8f6dc38beb1b3189a6491487b98f757d
Bryan <bryannsanchez1234@gmail.com.com>
2022-05-15 16:25:24 -0700

    support page css

--- client/src/pages/Support.js
+++ client/src/pages/Support.js
@@ -3,14 +3,15 @@ const Support = () => {
   return (
     <div>
       <h2>Support</h2>
-      <p>
-        NodeRun ensures that you can get your project up and running as soon as possible, contact one of our developers down below if you are still facing issues!
-      </p>
-      <p2>
-        Full-Stack Software Engineer:
+      <div className='support-container'>
+        <p className='support-quote'>
+          NodeRun ensures that you can get your project up and running as soon as possible, contact one of our developers down below if you are still facing issues!
+        </p>
+        <p2>
+          Full-Stack Software Engineer:
         <a href='https://www.linkedin.com/in/bryvn1xx/'>Bryan Sanchez</a>
-      </p2>
-
+        </p2>
+      </div>
 
 
 
